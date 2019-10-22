0d589c1625281dc951358f6ea55e0acd68f501fb
Mario_95 <mrojasc95.mr@gmail.com>
2019-10-22 15:18:33 -0400

    Giphy API key obtained and test request carried out successfully

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
 
 
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&limit=5";
+    // queryURL.done(function (data) {
+    //     console.log("success got data", data);
+    // });
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL)
+        console.log(response)
+
+    })
 
 
 
 
-
-
-
-
-        }
+})
