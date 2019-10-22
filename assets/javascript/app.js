c2ee1966ead845f9583f666fa950a57e4d71df4c
Mario_95 <mrojasc95.mr@gmail.com>
2019-10-22 18:33:46 -0400

    rows an colums modified, searchbar added from bootstrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,7 @@ $(document).ready(function () {
 
     let tvShows = ["Madmen", "Doctor Who", "30 Rock", "Barry"]
 
-    let queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&tag=cats";
-
-    // trial API request (funtionla but gives nack the same GIFS )
-    "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&limit=5";
-
+    let queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&tag=" + tvShows + "&limit=4";
 
     $.ajax({
         url: queryURL,
