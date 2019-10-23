031161255dfae089ac0445cc323649972c6f8704
Mario_95 <mrojasc95.mr@gmail.com>
2019-10-23 18:00:22 -0400

    functionality updated, btns added dynamically from user input in search bar, API request functionality to be redone

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,53 +5,46 @@ $(document).ready(function () {
 
     let l = tvShows.length;
 
-    for (let a = 0; a < 2; a++) {
+    // for (let a = 0; a < 10; a++) {
 
-        for (let i = 0; i < l; i++) {
-            // globally replace blank spaces with "" in order to join the name of shows more
-            // more thank 1 word long.
-            // put in  request for each show in the array.
-            let show = tvShows[i].replace(/ /g, "")
-            let queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&tag=" + show + '"';
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(queryURL)
-                console.log(response)
-            })
+    //     for (let i = 0; i < l; i++) {
+    // globally replace blank spaces with "" in order to join the name of shows more
+    // more thank 1 word long.
+    // put in  request for each show in the array.
+    //         let show = tvShows[i].replace(/ /g, "")
+    //         let queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&tag=" + show + '"';
+    //         $.ajax({
+    //             url: queryURL,
+    //             method: "GET"
+    //         }).then(function (response) {
+    //             console.log(queryURL)
+    //             console.log(response)
+    //         })
 
-        }
-    }
+    //     }
+    // }
 
     $("#addNewMovie").on("click", function (event) {
         //  prevent page from resetting when the search btn is clicked 
         event.preventDefault()
         // extract the vule of the search input 
-        // if the show already exist in the array, alert user
+        // if the show already exist in the array or no input is given, alert user
         // otherwise push it to array of shows  
         let newShow = $("#searchbar").val().trim()
-        if (tvShows.includes(newShow)) {
+        if (newShow.length === 0) {
+            alert("please enter a tv show")
+        } else if (tvShows.includes(newShow)) {
             alert("show already created")
         } else {
-            tvShows.push(newShow);
+            tvShows.push(newShow)
+            // try to clear the value of the search bar once the item has been added 
+            // $(".form-control").clear()
+            let newBtn = $("<button>")
+            newBtn.attr("id", newShow).text(newShow)
+            newBtn.attr("class", "btn")
+            $("#buttons-appear-here").prepend(newBtn)
             console.log(tvShows);
         }
-
-        // create a loop to make multiple request for each show in array (put this on the on)
-
-        // let queryURL = "http://api.giphy.com/v1/gifs/random?&api_key=bGj6DeJcRbOsxXYoAoQNgYUmJE7TDgDB&tag=" + show + '"';
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // })
-
-        // newShow = document.getElementById("searchbar");
-        // $(newShow).value()
-
-        // let newShow = $("#searchbar").val().trim()
-
-
     })
 
     // for (let i = 0; i < 5; i++) {
@@ -60,7 +53,5 @@ $(document).ready(function () {
     //     )
 
     // }
-
-
 
 })
